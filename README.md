d6cf23ea2caa0f7891a04f5a43efb7b06ca132e2
JoaumVictor <94501287+JoaumVictor@users.noreply.github.com>
2024-01-16 11:22:05 -0300

    feat: finish him

--- README.md
+++ README.md
@@ -18,7 +18,6 @@ após baixar:
 
   - O input de switch está diferente do "Figma"
 
-- Está faltando a última tela de finalização após clicar em "Confirm" no quarto passo
 - Poderia ser usado React Context ao invés do local storage pra guardar os dados entre os steps
 - Bibliotecas foram instaladas e não utilizadas por falta de tempo "Framer Motion"
 - Faltam pequenas animações em botões e imagens que melhoram a experiência do usuário
--- src/components/steps/FourthStep/index.tsx
+++ src/components/steps/FourthStep/index.tsx
@@ -5,6 +5,8 @@ import plans from "../../../mock/plans";
 import options from "../../../mock/options";
 import { classNames } from "../../../util/shared";
 
+import thankyou from "../../../images/icon-thank-you.svg";
+
 interface FourthStepI {
   setStep: React.Dispatch<React.SetStateAction<FormSteps>>;
 }
@@ -48,76 +50,89 @@ function FourthStep({ setStep }: FourthStepI) {
       return acc;
     }, 0);
 
+  const [complete, setComplete] = useState(false);
+
   return (
     <div className="flex flex-col items-center justify-between w-[520px] h-[568px] px-20 pt-12 pb-6 rounded-xl">
-      <div className="flex flex-col items-center justify-center w-full">
-        <p className="text-3xl w-full text-[#01265A] font-ubuntu-bold mb-2">
-          Finishing Up
-        </p>
-        <p className="text-gray-400 text-[12px] w-full mb-4">
-          Double-check everything looks OK before confirming.
-        </p>
-        <div className="bg-gray-200 rounded-[8px] w-full mt-5">
-          <div className="flex flex-row items-center justify-between px-5 py-3">
-            <div>
-              <p className="text-[14px] font-ubuntu-bold text-[#01265A]">
-                {`${finallyPlan.plan} (${finallyPlan.period})`}
-              </p>
-              <p
-                onClick={() => setStep(2)}
-                className="underline text-[#01265A] hover:font-ubuntu-bold text-[12px] cursor-pointer"
-              >
-                Change
+      {!complete ? (
+        <div className="flex flex-col items-center justify-center w-full">
+          <p className="text-3xl w-full text-[#01265A] font-ubuntu-bold mb-2">
+            Finishing Up
+          </p>
+          <p className="text-gray-400 text-[12px] w-full mb-4">
+            Double-check everything looks OK before confirming.
+          </p>
+          <div className="bg-gray-200 rounded-[8px] w-full mt-5">
+            <div className="flex flex-row items-center justify-between px-5 py-3">
+              <div>
+                <p className="text-[14px] font-ubuntu-bold text-[#01265A]">
+                  {`${finallyPlan.plan} (${finallyPlan.period})`}
+                </p>
+                <p
+                  onClick={() => setStep(2)}
+                  className="underline text-[#01265A] hover:font-ubuntu-bold text-[12px] cursor-pointer"
+                >
+                  Change
+                </p>
+              </div>
+              <p className="text-[#01265A] font-ubuntu-bold text-[12px]">
+                $
+                {finallyPlan.period === "monthly"
+                  ? `${completePlan?.monthlyPrice}/mo`
+                  : `${completePlan?.yearlyPrice}/ye`}
               </p>
             </div>
-            <p className="text-[#01265A] font-ubuntu-bold text-[12px]">
-              $
+            <div className="w-[94%] h-[1px] mx-auto bg-gray-300" />
+            {options.map((option) => (
+              <div
+                id={option.title}
+                className={classNames(
+                  "flex items-center justify-between px-4 my-2",
+                  !finallyServices[option.optionName] && "hidden"
+                )}
+              >
+                <p className="text-[12px] text-gray-400">{option.title}</p>
+                <p className="text-[12px] text-[#01265A]">
+                  {finallyPlan.period === "monthly"
+                    ? `+${option.price}/mo`
+                    : `+${option.price * 12}/ye`}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="flex items-center justify-between w-full mt-10">
+            <p className="text-[12px] text-gray-400">
               {finallyPlan.period === "monthly"
-                ? `${completePlan?.monthlyPrice}/mo`
-                : `${completePlan?.yearlyPrice}/ye`}
+                ? "Total (per month)"
+                : "Total (per year)"}
+            </p>
+            <p className="text-[16px] text-[#5145E6] font-ubuntu-bold">
+              {finallyPlan.period === "monthly"
+                ? `+${totalValue}/mo`
+                : `+${totalValue * 12}/ye`}
             </p>
           </div>
-          <div className="w-[94%] h-[1px] mx-auto bg-gray-300" />
-          {options.map((option) => (
-            <div
-              id={option.title}
-              className={classNames(
-                "flex items-center justify-between px-4 my-2",
-                !finallyServices[option.optionName] && "hidden"
-              )}
-            >
-              <p className="text-[12px] text-gray-400">{option.title}</p>
-              <p className="text-[12px] text-[#01265A]">
-                {finallyPlan.period === "monthly"
-                  ? `+${option.price}/mo`
-                  : `+${option.price * 12}/ye`}
-              </p>
-            </div>
-          ))}
         </div>
-        <div className="flex items-center justify-between w-full mt-10">
-          <p className="text-[12px] text-gray-400">
-            {finallyPlan.period === "monthly"
-              ? "Total (per month)"
-              : "Total (per year)"}
-          </p>
-          <p className="text-[16px] text-[#5145E6] font-ubuntu-bold">
-            {finallyPlan.period === "monthly"
-              ? `+${totalValue}/mo`
-              : `+${totalValue * 12}/ye`}
-          </p>
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full h-full gap-4">
+          <img src={thankyou} alt="thanks" />
+          <p className="text-3xl text-[#322c7f] font-ubuntu-bold">Thank You</p>
+          <p className="text-[#322c7f]">Tamo junto 🤌✨</p>
         </div>
-      </div>
-      <div className="flex flex-row items-center justify-between w-full flex-nowrap">
-        <Button label="Go Back" type="secondary" onClick={() => setStep(3)} />
-        <button
-          className={classNames(
-            "bg-[#938CFD] disabled:bg-[#263c57fd] text-white rounded-[8px] mt-4 px-4 py-2 text-[12px]"
-          )}
-        >
-          Confirm
-        </button>
-      </div>
+      )}
+      {!complete && (
+        <div className="flex flex-row items-center justify-between w-full flex-nowrap">
+          <Button label="Go Back" type="secondary" onClick={() => setStep(3)} />
+          <button
+            className={classNames(
+              "bg-[#938CFD] disabled:bg-[#263c57fd] text-white rounded-[8px] mt-4 px-4 py-2 text-[12px]"
+            )}
+            onClick={() => setComplete(true)}
+          >
+            Confirm
+          </button>
+        </div>
+      )}
     </div>
   );
 }
